abd0af0060d235daa98c8393487c791a1e9c899f
Jason Anh Vu <jvu@scu.edu>
2020-09-06 10:36:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // ASSIGN VARIABLES
 const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], autoConnect: true } );
 const config = require('./config.json');
 const Enmap = require("enmap");
 const fs = require("fs");
+
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
-client.config = config;
+client.config = require(`./config.json`);
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
@@ -26,7 +31,6 @@ fs.readdir("./commands/", (err, files) => {
     if (!file.endsWith(".js")) return;
     let props = require(`./commands/${file}`);
     let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName} ✅`);
     client.commands.set(commandName, props);
   });
 });
