d0dcf95c4a793b9b80d1a71b6bc9e801e9eefd6c
Jason Anh Vu <jvu@scu.edu>
2020-09-19 10:45:03 -0700

    Update covid19.js

--- commands/covid19.js
+++ commands/covid19.js
@@ -1,12 +1,12 @@
 const { MessageEmbed } = require('discord.js');
 const fetch = require(`node-fetch`);
-const { prefix, school_color } = require('../config.json');
+const config = require('../config.json');
 
 module.exports = {
     name: 'covid19', //forked from Raptor SA
     description: 'Get daily and instant COVID-19 data here!', //here is a change in the file
-    usage: `${prefix}covid19 global __**OR**__ ${prefix}covid19 [state abbreviation] __**OR**__ ${prefix}covid19 county`,
-    guildOnly: false,
+    args: true,
+    usage: `global / ${config.prefix}covid19 [state abbreviation] / ${config.prefix}covid19 county`, 
         async execute (message, args) {
             message.delete();
 
@@ -38,7 +38,7 @@ module.exports = {
             COVID_EMBED1.setTimestamp()
             COVID_EMBED1.attachFiles(`./assets/covid-19.jpg`)
             COVID_EMBED1.setImage(`attachment://covid-19.jpg`)
-            COVID_EMBED1.setColor(school_color)
+            COVID_EMBED1.setColor(config.school_color)
 
             const COVID_EMBED2 = new MessageEmbed()
             COVID_EMBED2.setTitle(`__**USA COVID-19 Status Tracker by State:**__`)
@@ -75,12 +75,12 @@ module.exports = {
             COVID_EMBED3.addField(`Fatality Rate: `, data3.message[9].fatality_rate, true)
             COVID_EMBED3.addField(`\u200B`, `\u200B`, true)
             COVID_EMBED3.setFooter(`Brought to you by the server lords on ${targetDate}`)
-            COVID_EMBED3.setColor(school_color)
+            COVID_EMBED3.setColor(config.school_color)
             
             if (args[0] === "global") return message.channel.send(COVID_EMBED1).catch(err => `Error: ${err}`)
-            if (args[0].length >= 2) { 
+            if (args[0].length == 2) { 
                 message.channel.send(COVID_EMBED2).catch(err => `Error: ${err}`)
-            } else if (args[0].length < 2) {
+            } else {
                 message.channel.send({embed: { description: "That was not a state abbreviation. Please try again!", color: 10231598}})
             }
             if (args[0] === "county") return message.channel.send(COVID_EMBED3).catch(err => `Error: ${err}`)
