0beb4e049dee00771269d8858017e33e88c082ff
Jason Anh Vu <jvu@scu.edu>
2020-09-25 11:28:44 -0700

    Delete errorMod.js

--- modules/errorMod.js
+++
@@ -1,4 +0,0 @@
-module.exports = function errorEvent(type, message) {
-	message.channel.send({ embed: { title: `:x: Missing Permissions!`, description: `:no_entry_sign: | ${type}`}});
-	message.channel.stopTyping(true);
-};
