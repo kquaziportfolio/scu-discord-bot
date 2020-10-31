3b12608148b4126c22de3319fc2bfff8da561552
Jason Vu <jvu@scu.edu>
2020-10-30 23:21:56 -0700

    Update rank.js

--- commands/fun/rank.js
+++ commands/fun/rank.js
@@ -17,19 +17,19 @@ module.exports = {
       const usersRef = db.collection(message.guild.name);
       const users = await usersRef.orderBy('karma', 'desc').get();
 
-      let leaderboard = '';
+      let scoreboard = '';
       let place = 1;
       await users.docs.forEach(async user => {
         const userData = user.data();
         message.guild.members.fetch(userData.id).then(guildMember => {
-          leaderboard += `${place}. **${guildMember.user.username} :** ${userData.karma}\n`;
+          scoreboard += `${place}. **${guildMember.user.username} :** ${userData.karma}\n`;
           place += 1;
         });
       });
 
       const embed = new MessageEmbed()
         .setTitle('Rank Leaderboard')
-        .setDescription(leaderboard)
+        .setDescription(scoreboard)
         .setColor(client.config.school_color)
 
       await message.channel.send(
