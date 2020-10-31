351a357d6dcffa494cb035d8b3a7118f0bd21ab3
Jason Vu <jvu@scu.edu>
2020-10-31 13:14:41 -0700

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -11,20 +11,19 @@ module.exports = {
     usage: `<new prefix>`,  
     async execute (client, message, args) {
         if(isAdmin(client, message, false)) {
-            if (client.config.prefix !== prefixConf) {
-                let finalConf = client.config;
-                finalConf.prefix = prefixConf;
-                fs.writeFile(`../../config.json`, JSON.stringify(finalConf, null, 3), (err) => {
-                    if (err) throw err;
-                });
-            } else {
-                message.channel.send("That already is the current prefix!");   
-            }
+            let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8")); //Read File
+              prefixes[message.guild.id] = { //Let The config be
+                prefix: args[0] //Let prefix = arguement 1
+              }
+
+              fs.writeFile("./prefix.json", JSON.stringify(prefixes), (err) => { //Write File
+                if(err) console.log(err); //If error log error to the console
+              });
 
             let newPrefix = new MessageEmbed() 
-            .setColor(client.config.school_color)
-            .setTitle(`**NEW PREFIX SET**`)
-            .setDescription(`Set to \`${prefixConf}\``)
+                .setColor(client.config.school_color)
+                .setTitle(`**NEW PREFIX SET**`)
+                .setDescription(`Set to \`${args[0]}\``)
 
             message.channel.send(newPrefix);
         }
