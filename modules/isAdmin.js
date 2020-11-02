935b53b9c8429fe33b5e15183ecbfe1a09defabf
Jason Vu <jvu@scu.edu>
2020-11-02 11:52:13 -0800

    Update isAdmin.js

--- modules/isAdmin.js
+++ modules/isAdmin.js
@@ -15,10 +15,9 @@
 module.exports = async function isAdmin(client, message, statement) {
   
   let error = require("./missingPerms.js");
-    if (message.member.roles.cache.has(client.config.serverRoles.owner || client.config.serverRoles.admin || client.config.serverRoles.mod)) {
+    if (message.member.roles.cache.has(client.config.serverRoles.owner) || message.member.roles.cache.has(client.config.serverRoles.admin) || message.member.roles.cache.has(client.config.serverRoles.mod)) {
       return true; 
     } else if (statement == true) {
         error(`You are missing the **OWNER**, **ADMIN**, or **MOD** permission roles.`, message);
-        return false;
     }
 };
