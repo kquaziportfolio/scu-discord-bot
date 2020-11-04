714518f0536f742866ccb35ec880f885c38b52c2
Jason Vu <jvu@scu.edu>
2020-11-03 16:30:03 -0800

    Update dm.js

--- commands/admin/dm.js
+++ commands/admin/dm.js
@@ -1,4 +1,5 @@
 let isAdmin = require(`../../modules/isAdmin.js`);
+let error = require(`../../modules/
 
 module.exports  = {
     name: 'dm',
@@ -8,7 +9,7 @@ module.exports  = {
     category: 'Admin',  
     async execute(client, message, args) {
         
-        if(isAdmin(client, message, true)) {
+        if(isAdmin(client, message, false)) {
             if (args[0] == null) {
                 return message.channel.send({ embed: { title: `Here's an example:`, description: `${client.config.prefix}dm [@user] [message]`, color: client.config.school_color}});
               } else {
@@ -18,7 +19,7 @@ module.exports  = {
                     member.send(msgSender);
                     message.channel.stopTyping(true);
                 } catch (e) {
-                    error("There was an error sending that DM", message);
+                    console.log("There was an error sending that DM", e);
                 }
             }
         }  
