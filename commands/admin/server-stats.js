abf8de3da5968a3ea2644d87c3a6c1063dc64871
Jason Vu <jvu@scu.edu>
2020-11-04 09:49:24 -0800

    Update server-stats.js

--- commands/admin/server-stats.js
+++ commands/admin/server-stats.js
@@ -1,50 +1,48 @@
 const { MessageEmbed } = require(`discord.js`); //for embed functionality
+let isAdmin = require(`../../modules/isAdmin.js`);
+let sendMessage = require(`../../modules/sendMessage.js`);
 
 module.exports = {
-	name: 'server-stats',
+    name: 'server-stats',
     description: 'Get general server statistics!',
     category: 'Admin',  
-		async execute(client, message, args) {
+    async execute(client, message, args) {
+	    if(isAdmin(client, message, true)) {
+		message.delete();
 
-            let isAdmin = require(`../../modules/isAdmin.js`);
-            let sendMessage = require(`../../modules/sendMessage.js`);
+		function checkBots(guild) {
+		    let botCount = 0;
+		    guild.members.cache.forEach(member => {
+			if(member.user.bot) botCount++;
+		    });
+		    return botCount;
+		}
 
-            if(isAdmin(client, message, false)) {
-                message.delete();
+		function checkMembers(guild) {
+		    let memberCount = 0;
+		    guild.members.cache.forEach(member => {
+			if(!member.user.bot) memberCount++;
+		    });
+		    return memberCount;
+		}
 
-                function checkBots(guild) {
-                    let botCount = 0;
-                    guild.members.cache.forEach(member => {
-                        if(member.user.bot) botCount++;
-                    });
-                    return botCount;
-                }
+		let sicon = message.guild.iconURL();
+		let serverembed = new MessageEmbed()
+		    .setDescription(`__**${message.guild.name} - Statistics**__`)
+		    .setColor(client.config.school_color)
+		    .addField('Server Owner', `<@${client.config.serverRoles.owner}>`, true)
+		    .addField('Server Region', message.guild.region, true)
+		    .setThumbnail(sicon, true)
+		    .addField("Server Name", message.guild.name, true)
+		    .addField('Verification level', message.guild.verificationLevel, true)
+		    .addField('Channel Count', message.guild.channels.cache.size, true)
+		    .addField('Total Member Count', message.guild.memberCount, true)
+		    .addField('Humans', checkMembers(message.guild), true)
+		    .addField('Bots', checkBots(message.guild), true)
+		    .addField('Guild Created At:', message.guild.createdAt, true)
+		    .setTimestamp()
 
-                function checkMembers(guild) {
-                    let memberCount = 0;
-                    guild.members.cache.forEach(member => {
-                        if(!member.user.bot) memberCount++;
-                    });
-                    return memberCount;
-                }
-
-                let sicon = message.guild.iconURL();
-                let serverembed = new MessageEmbed()
-                    .setDescription(`__**${message.guild.name} - Statistics**__`)
-                    .setColor(client.config.school_color)
-                    .addField('Server Owner', `<@${client.config.serverRoles.owner}>`, true)
-                    .addField('Server Region', message.guild.region, true)
-                    .setThumbnail(sicon, true)
-                    .addField("Server Name", message.guild.name, true)
-                    .addField('Verification level', message.guild.verificationLevel, true)
-                    .addField('Channel Count', message.guild.channels.cache.size, true)
-                    .addField('Total Member Count', message.guild.memberCount, true)
-                    .addField('Humans', checkMembers(message.guild), true)
-                    .addField('Bots', checkBots(message.guild), true)
-                    .addField('Guild Created At:', message.guild.createdAt, true)
-                    .setTimestamp()
-
-                sendMessage(client, client.config.channels.auditlogs, serverembed);
-            }
-        } 
+		sendMessage(client, client.config.channels.auditlogs, serverembed);
+	    }
+	} 
 }
