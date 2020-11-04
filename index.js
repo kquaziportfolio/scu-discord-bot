e9486b523ccf4d7ebdd1c74b4a5934659a72ec4b
Jason Vu <jvu@scu.edu>
2020-11-03 21:37:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], autoConnect: true } );
 const Enmap = require("enmap");
 const fs = require("fs");
-const { readdirSync } = require("fs");
-const { join } = require("path");
 
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = require(`./config.json`);
