4d11207a17d65563d8c773334d5ad773c7fe9168
Jason Vu <jvu@scu.edu>
2020-11-07 22:41:53 -0800

    Update express.js

--- commands/admin/express.js
+++ commands/admin/express.js
@@ -4,18 +4,22 @@ let isAdmin = require(`../../modules/isAdmin.js`);
 
 module.exports = { 
     name: 'express', //here is a change in the file
-    description: 'Check status of a server via an endpoint!',
+    description: 'Check status of the verification server via an endpoint!',
     category: 'Admin',
     async execute(client, message, args) {
 
         if(isAdmin(client, message, true)) {
             const url = client.config.verification.verifyURL;
-            const status = await fetch(url)
+            const status = await fetch(url);
             const getStatus = await status.text();
-
+            
+            if (!status.ok) {
+                sendMessage(client, client.channels.auditlogs, { embed: { title: `Error :x:`, description: `Go to [https://pitunnel.com](PiTunnel) and reset using the following commands: \`${client.config.verification.mappingRule}\``, color: client.config.school_color}});
+            }
+            
             const statusEmbed = new MessageEmbed()
             .setTitle(` VERIFICATION SERVER`)
-            .setDescription(`${getStatus} - Check the status [here](${url})! :white_check_mark:`)
+            .setDescription(`${getStatus} - Check the status [here](${url})!`)
             .setColor(client.config.school_color)
 
             message.channel.send(statusEmbed);
