48adc0706e0fc01a7001f896692357c52a7da7f2
Jason Vu <jvu@scu.edu>
2020-11-08 22:32:25 -0800

    Update server-roles.js

--- commands/admin/server-roles.js
+++ commands/admin/server-roles.js
@@ -12,7 +12,7 @@ module.exports = {
 		.setColor(client.config.school_color)
 		.setDescription(
 		`Please fill out this super-duper quick little [**form**](${client.config.verification.googleFormLink}) with your SCU email to instantly get the __**Student**__, graduating year, and major/program of study roles! All data is anonymous!\n\n` +
-		`**NOTE**: If you are a <@&${client.config.serverRoles.guest}> or <@&${client.config.serverRoles.prospectiveStudent}>, please contact <@&${client.config.serverRoles.admin}> or <@&${client.config.serverRoles.mod}> to give you roles as the form is for SCU domain users! Thank you for your cooperation! 😊`
+		`**NOTE**: If you are a <@&${client.config.serverRoles.prospectiveStudent}>, please contact <@&${client.config.serverRoles.admin}> or <@&${client.config.serverRoles.mod}> to give you roles as the form is for SCU domain users! Thank you for your cooperation! 😊`
 		)
 		.attachFiles([`./assets/bronco_statue.jpg`])
 		.setImage(`attachment://bronco_statue.jpg`)
