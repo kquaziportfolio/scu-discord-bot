0624a68842e415f500a827f6611331ce5a6e2806
Jason Vu <jvu@scu.edu>
2020-11-09 09:56:02 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,7 +5,7 @@ const active = new Map();
 
 module.exports = async (client, message) => {
   // Checks if the Author is a Bot, or the message isn't from the guild, ignore it.
-  if (!message.content.startsWith(client.config.prefix) || message.author.bot) return;
+  if (!message.content.startsWith(client.config.prefix) && message.channel.type != "dm" || message.author.bot) return;
   
   //Check if message is in a direct message
   if (message.guild == null) {
