64abaff1d10fb721209f24c057923119966cc627
Jason Vu <jvu@scu.edu>
2020-11-09 16:57:06 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
 let sendMessage = require(`../modules/sendMessage.js`);
 const fetch = require(`node-fetch`);
-let status = require(`../modules/status.js`);
 
 module.exports = async (client) => {
 	try {
@@ -15,15 +14,13 @@ module.exports = async (client) => {
 		};
 		console.log(verifyMSG.description);
 		sendMessage(client, client.config.channels.auditlogs, { embed: verifyMSG});
-		
+
 		let memberCount = guild.members.cache.filter(member => !member.user.bot).size;	
 		let liveCount = guild.channels.cache.find(channel => channel.id === client.config.channels.memberCount);
 		liveCount.setName(`👥 ${memberCount} Members`);
 
-		setInterval(function() {
-			client.user.setPresence({activity: { name: status.getStatus() }, status: 'online'})
-		}, 20000); //change status every 20 seconds to avoid API abuse :)
-		
+		client.user.setPresence({activity: { name: `${client.config.prefix}help || DM the bot for mod support! 📩` }, status: 'online'});
+
 		/* DISCORD STATUS CHECKER */
 		const url = client.config.verification.status;
 		const response = await fetch(url);
@@ -37,7 +34,7 @@ module.exports = async (client) => {
 			sendMessage(client, client.config.channels.auditlogs, { embed: { title: `${body.status.description}`, description: "Check the status [here](https://discordstatus.com/)! :white_check_mark:", color: "GREEN", timestamp: new Date()}});
 		} else {
 			sendMessage(client, client.config.channels.auditlogs, { embed: { title: `:x: ${body.status.description}`, description: "There seems to be an error with some of the Discord servers. Double check [here](https://status.discordapp.com/)! :x:", color: "RED", timestamp: new Date()}});
-		}
+                }
 	} catch (err) {
 		console.log(err);
 	}
