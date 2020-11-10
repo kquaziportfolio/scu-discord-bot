1f7641186ba0c2f9c7cc9dd78a92dfd4c7b31b88
Jason Vu <jvu@scu.edu>
2020-11-09 16:25:32 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -75,7 +75,7 @@ module.exports = async (client, message) => {
         if (!supportUser) return message.channel.delete();
         
         const modRole = client.config.serverRoles;
-        if (message.content == `${client.config.prefix}close-ticket` && message.member.roles.cache.some(r=>["Owner", "Admin", "Mod"].includes(r.name))) {
+        if (message.content == `${client.config.prefix}close-ticket` && message.member.roles.cache.has(r => (r.name == "Owner") || (r.name == "Admin") || (r.name == "Mod"))) {
           const completeTicket = new MessageEmbed()
             .setColor(client.config.school_color)
             .setTitle(`ModMail Ticket Resolved`)
