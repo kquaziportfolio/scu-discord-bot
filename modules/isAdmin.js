4c8d4c3e5a32a1267e93232fb25b737882a7ae91
Jason Vu <jvu@scu.edu>
2020-11-10 23:37:32 -0800

    Update isAdmin.js

--- modules/isAdmin.js
+++ modules/isAdmin.js
@@ -17,12 +17,11 @@ module.exports = async function isAdmin(client, message, statement) {
   const modRole = client.config.serverRoles;
   if(message.member.roles.cache.some(r=>["Owner", "Admin", "Mod"].includes(r.name))) {
    console.log(`User is either an Owner, Admin, or Mod`); 
-   return true;
   } else {
      if (statement == true) {
       console.log(`User is not an Owner, Admin, or Mod`); 
       error(`You don't have one of the following roles: \`OWNER\`, \`ADMIN\`, \`MOD\``, message);
-      return;
      }
+  return;
   }
 }
