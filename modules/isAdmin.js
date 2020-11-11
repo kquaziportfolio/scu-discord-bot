5880d67872ce579b94273fc4e508f8fb985c2d18
Jason Vu <jvu@scu.edu>
2020-11-10 23:42:25 -0800

    Update isAdmin.js

--- modules/isAdmin.js
+++ modules/isAdmin.js
@@ -16,11 +16,9 @@ module.exports = async function isAdmin(client, message, statement) {
   let error = require("./missingPerms.js");
   const modRole = client.config.serverRoles;
   if(!message.member.roles.cache.some(r=>["Owner", "Admin", "Mod"].includes(r.name))) {
-     if (statement == true) {
-      console.log(`User is not an Owner, Admin, or Mod`); 
-      error(`You don't have one of the following roles: \`OWNER\`, \`ADMIN\`, \`MOD\``, message);
-      return;
-     }
+     console.log(`User is not an Owner, Admin, or Mod`); 
+     error(`You don't have one of the following roles: \`OWNER\`, \`ADMIN\`, \`MOD\``, message);
+     if (statement == true) return;
   } else {
      console.log(`User is either an Owner, Admin, or Mod`);
   }
