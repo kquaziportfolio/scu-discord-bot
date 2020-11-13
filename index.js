f0f6819df99140cb371a8b1a313afb5d701755be
Jason Vu <jvu@scu.edu>
2020-11-13 09:16:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -24,16 +24,61 @@ fs.readdir("./events/", (err, files) => {
 
 client.commands = new Enmap(); 
 
-const commandFiles = fs.readdirSync(`./commands/`).filter(file => file.endsWith('.js'));
+// ASSIGN VARIABLES
+const Discord = require('discord.js');
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], autoConnect: true } );
+const Enmap = require("enmap");
+const fs = require("fs");
 
-for (const file of commandFiles) {
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = require(`./config.json`);
+
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
+client.on("error", console.error);
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
     if (!file.endsWith(".js")) return;
-    const subdirectories = ['admin', 'fun', 'utility'];
-    for (const subdirectory of subdirectories) {
-      const command = require(`./commands/${subdirectory}/${file}`);
-      client.commands.set(command.name, command);
-    }
-}
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/admin", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/admin/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+});
+
+fs.readdir("./commands/utility", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/utility/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+});
+
+fs.readdir("./commands/fun", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/fun/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+}); 
 
 // BOT TOKEN
 client.login(client.config.token);
