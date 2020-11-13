4e6ec6645430fa6817cc65ae47add067ba1d67bf
Jason Vu <jvu@scu.edu>
2020-11-13 08:34:25 -0800

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -5,9 +5,10 @@ let sendMessage = require(`../modules/sendMessage.js`);
 module.exports = async (client, member) => {
 	if(member.user.bot) return; //ignore members who are bot users
 	
-    const guild = client.guilds.cache.get(`${client.config.verification.guildID}`);
-
-let memberCount = guild.members.cache.filter(member => !member.user.bot).size;
+	const guild = client.guilds.cache.get(`${client.config.verification.guildID}`);
+	let memberCount = guild.members.cache.filter(member => !member.user.bot).size;	
+	let liveCount = guild.channels.cache.find(channel => channel.id === client.config.channels.memberCount);
+	liveCount.setName(`👥 ${memberCount} Members`);
 	
 	let leaveEmbed = new MessageEmbed() // Creating instance of Discord.MessageEmbed()
 	.setDescription(`<@${member.user.id}> has left **${guild.name}** which now has ${memberCount} members!`) //Setting embed description
