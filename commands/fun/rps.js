c300b4dcc9c92914a06149a7976405a9f3de9c57
Jason Vu <jvu@scu.edu>
2020-11-12 19:08:33 -0800

    Update rps.js

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -1,5 +1,5 @@
 module.exports =  {  
-	name: 'rps',
+    name: 'rps',
     description: 'Play rock, paper, scissors!',
     args: true,
     usage: `[Choose rock, paper, or scissors]`, 
@@ -10,12 +10,16 @@ module.exports =  {
         const USER_PLAYER = `**${message.author}** wins this round!`;
         const COMPUTER_PLAYER = `**COMPUTER** wins this round!`;
         const TIE = `It's a tie!`;
-        if (!args[0]) return message.channel.send({ embed: {description: "Choose a move: \`\`\`rock, paper, or scissors \`\`\`", color: client.config.school_color}});
+		
+        if (!args[0]) message.channel.send({ embed: {description: "Choose a move: \`\`\`rock, paper, or scissors \`\`\`", color: client.config.school_color}});
+		
         if (choices.indexOf(args[0]) == -1) {
             return message.channel.send({embed: {description: "Choose a move: \`\`\`rock, paper, or scissors \`\`\`", color: client.config.school_color}});
         }
+		
         let computer = choices[Math.floor(Math.random() * 3 + 1) - 1];
         let user = args[0];
+		
         function calculate(user, computer) {
             if (user == "rock" && computer == "scissors" || user == "paper" && computer == "rock" || user == "scissors" && computer == "paper") {
                 return USER_PLAYER;
@@ -25,6 +29,7 @@ module.exports =  {
                 return TIE;
             }
         }
+		
         const rpsEmbed = {
             description: calculate(user, computer),
             title: `__**Your Results**__`,
