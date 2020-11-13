1093143439533d813b78f9f7933e18ac43777024
Jason Vu <jvu@scu.edu>
2020-11-13 08:37:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], autoConnect: true } );
 const Enmap = require("enmap");
+const fs = require("fs");
 const { readdirSync } = require("fs");
 const { join } = require("path");
 
