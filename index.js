c0acb7b0d4b7e014492d7af40c7e3de30d4f4d3d
Jason Vu <jvu@scu.edu>
2020-11-13 09:17:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], autoConnect: true } );
 const Enmap = require("enmap");
-const fs = require("fs"); 
-
-// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
-client.config = require(`./config.json`);
-
-client.on("warn", (e) => console.warn(e));
-client.on("debug", (e) => console.info(e));
-client.on("error", console.error);
-
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-    delete require.cache[require.resolve(`./events/${file}`)];
-  });
-});
-
-client.commands = new Enmap(); 
-
-// ASSIGN VARIABLES
-const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], autoConnect: true } );
-const Enmap = require("enmap");
 const fs = require("fs");
 
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
