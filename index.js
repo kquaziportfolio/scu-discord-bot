382d352cb94b25c9b1f898be361aeb4396613d39
Jason Vu <jvu@scu.edu>
2020-11-13 08:36:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], autoConnect: true } );
 const Enmap = require("enmap");
-const fs = require("fs");
+const { readdirSync } = require("fs");
+const { join } = require("path");
 
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = require(`./config.json`);
@@ -24,34 +25,19 @@ fs.readdir("./events/", (err, files) => {
 
 client.commands = new Enmap();
 
-fs.readdir("./commands/admin", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/admin/${file}`);
-    let commandName = file.split(".")[0];
-    client.commands.set(commandName, props);
-  });
-});
-
-fs.readdir("./commands/utility", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/utility/${file}`);
-    let commandName = file.split(".")[0];
-    client.commands.set(commandName, props);
-  });
-});
-
-fs.readdir("./commands/fun", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/fun/${file}`);
-    let commandName = file.split(".")[0];
-    client.commands.set(commandName, props);
-  });
+readdirSync(join(__dirname, "..")).forEach(f => {
+  const files = readdirSync(join(__dirname, "..", f));
+  if (files.includes(`${commandName}.js`)) {
+    const file = `../${f}/${commandName}.js`;
+    try {
+        delete require.cache[require.resolve(file)];
+        message.client.commands.delete(commandName);
+        const pull = require(file);
+        message.client.commands.set(commandName, pull);
+    } catch (e) {
+        return console.log(e.stack || e);
+    }
+  }
 });
 
 // BOT TOKEN
