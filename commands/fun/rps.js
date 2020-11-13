146ac14c8499369ff2f1cd6b3fbcf18d4186914e
Jason Vu <jvu@scu.edu>
2020-11-13 10:26:12 -0800

    Update rps.js

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -6,43 +6,25 @@ module.exports =  {
     category: 'Fun',
 	async execute(client, message, args) {
         
-        let choices = ["rock", "paper", "scissors"];
-        const USER_PLAYER = `**${message.author}** wins this round!`;
-        const COMPUTER_PLAYER = `**COMPUTER** wins this round!`;
-        const TIE = `It's a tie!`;
-		
-        if (!args[0]) message.channel.send({ embed: {description: "Choose a move: \`\`\`rock, paper, or scissors \`\`\`", color: client.config.school_color}});
-		
-        if (choices.indexOf(args[0]) == -1) {
-            return message.channel.send({embed: {description: "Choose a move: \`\`\`rock, paper, or scissors \`\`\`", color: client.config.school_color}});
-        }
+        let choices = ["rock", "paper", "scissors"]; 
 		
         let computer = choices[Math.floor(Math.random() * 3 + 1) - 1];
         let user = args[0];
 		
         function calculate(user, computer) {
             if (user == "rock" && computer == "scissors" || user == "paper" && computer == "rock" || user == "scissors" && computer == "paper") {
-                return USER_PLAYER;
+                return `**${message.author}** wins this round!`;
             } else if (computer == "rock" && user == "scissors" || computer == "paper" && user == "rock" || computer == "scissors" && user == "paper") {
-                return COMPUTER_PLAYER;
+                return `**<@${client.user.id}>** wins this round!`;
             } else {
-                return TIE;
+                return `It's a tie!`;
             }
         }
 		
         const rpsEmbed = {
             description: calculate(user, computer),
             title: `__**Your Results**__`,
-            fields: [
-                {
-                name: "User Choice",
-                value: user,
-                },
-                {
-                name: "Computer Choice",
-                value: computer,
-                },
-            ],
+            fields: [ { name: "User Choice", value: user, }, { name: "Computer Choice", value: computer, }, ],
             color: client.config.school_color
         };
         message.channel.send({ embed: rpsEmbed });
