7b965eb49fc237e61bd1915e78c671d60506edd3
Jason Vu <jvu@scu.edu>
2020-11-13 22:11:41 -0800

    Update course-avail.js

--- commands/utility/course-avail.js
+++ commands/utility/course-avail.js
@@ -11,19 +11,21 @@ module.exports = {
     async execute (client, message, args) {
 
         try {
-            const departmentName = args[0].toLowerCase().split("|");
+            const departmentName = args[0].toLowerCase().split("|").join(" ");
             let response = await fetch(`https://www.scu.edu/apps/ws/courseavail/search/4120/ugrad/${departmentName}`);
             let data = await response.json(); 
             
             console.log(data);
+            
+            if (data.title == null) return message.reply("That's not a valid course or department");
 
             const courseSummary = new MessageEmbed() 
-                .setDescription(`__**Search Results for ${data.title || 'nothing'}__`)
+                .setDescription(`__**Search Results for ${data.title}**__`)
                 .addField(`Term`, (data.results[1].strm_descr || "nothing"), true)
                 .addField(`Subject Description`, (data.results[3].subject_descr || "nothing"), true) 
                 .addField(`Class Description`, (data.results[6].class_descr || "nothing"), true) 
                 .addField(`Meeting Days`, (data.results[7].mtg_days_1 || "nothing"), true)
-                .addField(`Meeting Times`, `(${data.results[9].mtg_time_beg_1}-${data.results[10].mtg_time_beg_1} || "nothing")`, true)
+                .addField(`Meeting Times`, `${data.results[9].mtg_time_beg_1}-${data.results[10].mtg_time_end_1}` || "nothing")`, true)
                 .addField(`Class Building`, (data.results[12].mtg_facility_1 || "nothing"), true)
                 .addField(`Instructor`, (data.results[13].instr_1 || "nothing"), true)
                 .addField(`Seats Remaining`, (data.results[20].seats_remaining || "nothing"), true)
