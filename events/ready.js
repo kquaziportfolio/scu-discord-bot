b7f4102dd16ccef2da331dadd12cc75d288b7ffd
Jason Vu <jvu@scu.edu>
2020-11-16 10:35:24 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -13,31 +13,28 @@ let sendMessage = require(`../modules/sendMessage.js`);
 const fetch = require(`node-fetch`);
 
 module.exports = async (client) => {
-	try {  
+	try {
 		client.user.setPresence({activity: { name: `${client.config.prefix}help || DM me for help! 📩` }, status: 'online'}) 
-		.then(
-			async function() {
-			require("../modules/serverVerification.js").run(client); //start server verification module in ready event 
-			require("../modules/scuReddit.js").run(client); //start reddit module in ready event 
-				
-			/* DISCORD STATUS CHECKER */
-			const url = client.config.verification.status;
-			const response = await fetch(url);
-			const body = await response.json();
 
-			if (!response.ok) {
-				throw Error("Error: DISCORD_STATUS_REQUEST. Please tell the bot author.");
-			}
+		require("../modules/serverVerification.js").run(client); //start server verification module in ready event 
+		require("../modules/scuReddit.js").run(client); //start reddit module in ready event 
 
-			if (body.status.description == "All Systems Operational") {
-				sendMessage(client, client.config.channels.auditlogs, { embed: { title: `${body.status.description}`, description: "Check the status [here](https://discordstatus.com/)! :white_check_mark:", color: "GREEN", timestamp: new Date()}});
-			} else {
-				sendMessage(client, client.config.channels.auditlogs, { embed: { title: `:x: ${body.status.description}`, description: "There seems to be an error with some of the Discord servers. Double check [here](https://status.discordapp.com/)! :x:", color: "RED", timestamp: new Date()}});
-			}
-				
-			sendMessage(client, client.config.channels.auditlogs, { embed: { title: `Hooray!`, description: "All commands and events work! :white_check_mark:", color: "GREEN", timestamp: new Date()}});
-		})
-		.catch(err => console.error(err));
+		/* DISCORD STATUS CHECKER */
+		const url = client.config.verification.status;
+		const response = await fetch(url);
+		const body = await response.json();
+
+		if (!response.ok) {
+			throw Error("Error: DISCORD_STATUS_REQUEST. Please tell the bot author.");
+		}
+
+		if (body.status.description == "All Systems Operational") {
+			sendMessage(client, client.config.channels.auditlogs, { embed: { title: `${body.status.description}`, description: "Check the status [here](https://discordstatus.com/)! :white_check_mark:", color: "GREEN", timestamp: new Date()}});
+		} else {
+			sendMessage(client, client.config.channels.auditlogs, { embed: { title: `:x: ${body.status.description}`, description: "There seems to be an error with some of the Discord servers. Double check [here](https://status.discordapp.com/)! :x:", color: "RED", timestamp: new Date()}});
+		}
+
+		sendMessage(client, client.config.channels.auditlogs, { embed: { title: `Hooray!`, description: "All commands and events work! :white_check_mark:", color: "GREEN", timestamp: new Date()}});
 	} catch (err) {
 		console.log(err);
 	}
