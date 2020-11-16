058289ef6acfe7e09abc5e6b4f28768bc7d26952
Jason Vu <jvu@scu.edu>
2020-11-16 07:51:33 -0800

    Update scuReddit.js

--- modules/scuReddit.js
+++ modules/scuReddit.js
@@ -21,13 +21,13 @@ setInterval(() => {
             lastTimestamp = post.data.created_utc;
 
             const scuRedditEmbed = new MessageEmbed()
-            .setColor(client.config.school_color);
-            .setTitle(`${post.data.link_flair_text ? `[${post.data.link_flair_text}] ` : ''}${entities.decodeHTML(post.data.title)}`);
-            .setURL(`https://redd.it/${post.data.id}`);
-            .setDescription(`${post.data.is_self ? entities.decodeHTML(post.data.selftext.length > 253 ? post.data.selftext.slice(0, 253).concat('...') : post.data.selftext) : ''}`);
-            .setThumbnail(validUrl.isUri(post.data.thumbnail) ? entities.decodeHTML(post.data.thumbnail) : null);
-            .setFooter(`${post.data.is_self ? 'self post' : 'link post'} by ${post.data.author}`);
-            .setTimestamp(new Date(post.data.created_utc * 1000));
+            .setColor(client.config.school_color)
+            .setTitle(`${post.data.link_flair_text ? `[${post.data.link_flair_text}] ` : ''}${entities.decodeHTML(post.data.title)}`)
+            .setURL(`https://redd.it/${post.data.id}`)
+            .setDescription(`${post.data.is_self ? entities.decodeHTML(post.data.selftext.length > 253 ? post.data.selftext.slice(0, 253).concat('...') : post.data.selftext) : ''}`)
+            .setThumbnail(validUrl.isUri(post.data.thumbnail) ? entities.decodeHTML(post.data.thumbnail) : null)
+            .setFooter(`${post.data.is_self ? 'self post' : 'link post'} by ${post.data.author}`)
+            .setTimestamp(new Date(post.data.created_utc * 1000))
 
             sendMessage(client, client.config.channels.updates, scuRedditEmbed);
           }
