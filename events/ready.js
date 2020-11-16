6f97f1d8aa05d7173493b09c6f515cd15c674ca1
Jason Vu <jvu@scu.edu>
2020-11-16 10:07:52 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,36 +3,41 @@ const fetch = require(`node-fetch`);
 
 module.exports = async (client) => {
 	try { 
-
-		require("../modules/serverVerification.js").run(client); //start server verification module in ready event 
-                require("../modules/scuReddit.js").run(client); //start reddit module in ready event 
-
-		const guild = client.guilds.cache.get(client.config.verification.guildID);
-		const verifyMSG = {
-			title: "HURRAY!",
-			description: "All commands and events work! ✅",
-			color: "GREEN",
-			timestamp: new Date()
-		};
-		console.log(verifyMSG.description);
-		sendMessage(client, client.config.channels.auditlogs, { embed: verifyMSG}); 
-
+		
 		client.user.setPresence({activity: { name: `${client.config.prefix}help || DM me for help! 📩` }, status: 'online'});
+		
+		client.login(client.config.token)
+		.then(
+			function() {
+			require("../modules/serverVerification.js").run(client); //start server verification module in ready event 
+			require("../modules/scuReddit.js").run(client); //start reddit module in ready event 
 
-		/* DISCORD STATUS CHECKER */
-		const url = client.config.verification.status;
-		const response = await fetch(url);
-		const body = await response.json();
+			const guild = client.guilds.cache.get(client.config.verification.guildID);
+			const verifyMSG = {
+				title: "HURRAY!",
+				description: "All commands and events work! ✅",
+				color: "GREEN",
+				timestamp: new Date()
+			};
+			console.log(verifyMSG.description);
+			sendMessage(client, client.config.channels.auditlogs, { embed: verifyMSG}); 
 
-		if (!response.ok) {
-			throw Error("Error: DISCORD_STATUS_REQUEST. Please tell the bot author.");
-		}
+			/* DISCORD STATUS CHECKER */
+			const url = client.config.verification.status;
+			const response = await fetch(url);
+			const body = await response.json();
 
-		if (body.status.description == "All Systems Operational") {
-			sendMessage(client, client.config.channels.auditlogs, { embed: { title: `${body.status.description}`, description: "Check the status [here](https://discordstatus.com/)! :white_check_mark:", color: "GREEN", timestamp: new Date()}});
-		} else {
-			sendMessage(client, client.config.channels.auditlogs, { embed: { title: `:x: ${body.status.description}`, description: "There seems to be an error with some of the Discord servers. Double check [here](https://status.discordapp.com/)! :x:", color: "RED", timestamp: new Date()}});
-                }
+			if (!response.ok) {
+				throw Error("Error: DISCORD_STATUS_REQUEST. Please tell the bot author.");
+			}
+
+			if (body.status.description == "All Systems Operational") {
+				sendMessage(client, client.config.channels.auditlogs, { embed: { title: `${body.status.description}`, description: "Check the status [here](https://discordstatus.com/)! :white_check_mark:", color: "GREEN", timestamp: new Date()}});
+			} else {
+				sendMessage(client, client.config.channels.auditlogs, { embed: { title: `:x: ${body.status.description}`, description: "There seems to be an error with some of the Discord servers. Double check [here](https://status.discordapp.com/)! :x:", color: "RED", timestamp: new Date()}});
+			}
+		})
+		.catch(err => console.error(err));
 	} catch (err) {
 		console.log(err);
 	}
