38408e5a33374fc508bb44bede2e18d46a5dc857
Jason Vu <jvu@scu.edu>
2020-11-17 10:59:39 -0800

    Update covid19.js

--- commands/utility/covid19.js
+++ commands/utility/covid19.js
@@ -1,7 +1,6 @@
 const { MessageEmbed } = require(`discord.js`);
 const fetch = require(`node-fetch`);
-const cheerio = require(`cheerio`);
-let sendMessage = require(`../../modules/sendMessage.js`);
+const cheerio = require(`cheerio`); 
   
 module.exports = {
     name: 'covid19',  
@@ -9,26 +8,25 @@ module.exports = {
     category: 'Utility',
     async execute (client, message, args) {  
         const response = await fetch(client.config.api.covidStats);
-        const body = await response.json();
-        if (response.ok) { 
-            const scuEmbed = new MessageEmbed()
-            .setColor(client.config.school_color)
-            .setAuthor("SCU On-Campus Testing Dashboard", client.user.avatarURL()) 
-            .setDescription(" Test Date | Number of Tests | *Number of Positive Tests | Positivity Rate")
-            .setTitle(`The following information reflects results from tests conducted each week on SCU's campus. Find more details on the Testing Protocol website.`)
-            .setURL(`https://www.scu.edu/preparedscu/health-and-safety/testing-protocol/`)
-            .setFooter(`*Includes students, faculty, and staff. Includes some individuals tested multiple times.`, client.user.avatarURL()) 
+        const body = await response.text();
+        
+        if(response.ok) {
+          const scuEmbed = new MessageEmbed()
+          .setColor(client.config.school_color)
+          .setAuthor("SCU On-Campus Testing Dashboard", client.user.avatarURL()) 
+          .setDescription(" Test Date | Number of Tests | *Number of Positive Tests | Positivity Rate")
+          .setTitle(`The following information reflects results from tests conducted each week on SCU's campus. Find more details on the Testing Protocol website.`)
+          .setURL(`https://www.scu.edu/preparedscu/health-and-safety/testing-protocol/`)
+          .setFooter(`*Includes students, faculty, and staff. Includes some individuals tested multiple times.`, client.user.avatarURL()) 
 
-            const $ = cheerio.load(body); 
+          const $ = cheerio.load(body); 
 
-            $('table[class="fixed_header"] > tbody > tr').each(function() {
-                let stats = $(this).text(); 
-                scuEmbed.addField(`\u200B`, stats, true);
-            });  
+          $('table[class="fixed_header"] > tbody > tr').each(function() {
+              let stats = $(this).text(); 
+              scuEmbed.addField(`\u200B`, stats, true);
+          });  
 
-            message.channel.send(scuEmbed);
-        } else {
-            sendMessage(client, client.config.channels.auditlogs, { embed: { description: `Couldn't retrieve data from the website. Check your code!`, color: client.config.school_color}}); 
-        }
+          message.channel.send(scuEmbed);
+        } 
     }
 }
