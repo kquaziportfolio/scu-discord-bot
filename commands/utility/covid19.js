410f469751b55da60ced825e8f31b47f5f0aac00
Jason Vu <jvu@scu.edu>
2020-11-17 10:37:32 -0800

    Update covid19.js

--- commands/utility/covid19.js
+++ commands/utility/covid19.js
@@ -1,31 +1,34 @@
 const { MessageEmbed } = require(`discord.js`);
-const request = require(`request`);
-const cheerio = require(`cheerio`); 
+const node = require(`node-fetch`);
+const cheerio = require(`cheerio`);
+let sendMessage = require(`../../modules/sendMessage.js`);
   
 module.exports = {
     name: 'covid19',  
     description: 'Get daily and instant COVID-19 data at SCU here...', //here is a change in the file
     category: 'Utility',
     async execute (client, message, args) {  
-        request(client.config.api.covidStats, function (err, resp, body) {
-            if (!err && resp.statusCode == 200) { 
-                const scuEmbed = new MessageEmbed()
-                .setColor(client.config.school_color)
-                .setAuthor("SCU On-Campus Testing Dashboard", client.user.avatarURL()) 
-                .setDescription(" Test Date | Number of Tests | *Number of Positive Tests | Positivity Rate")
-                .setTitle(`The following information reflects results from tests conducted each week on SCU's campus. Find more details on the Testing Protocol website.`)
-                .setURL(`https://www.scu.edu/preparedscu/health-and-safety/testing-protocol/`)
-                .setFooter(`*Includes students, faculty, and staff. Includes some individuals tested multiple times.`, client.user.avatarURL()) 
+        const response = await fetch(client.config.api.covidStats);
+        const body = await response.json();
+        if (response.ok) { 
+            const scuEmbed = new MessageEmbed()
+            .setColor(client.config.school_color)
+            .setAuthor("SCU On-Campus Testing Dashboard", client.user.avatarURL()) 
+            .setDescription(" Test Date | Number of Tests | *Number of Positive Tests | Positivity Rate")
+            .setTitle(`The following information reflects results from tests conducted each week on SCU's campus. Find more details on the Testing Protocol website.`)
+            .setURL(`https://www.scu.edu/preparedscu/health-and-safety/testing-protocol/`)
+            .setFooter(`*Includes students, faculty, and staff. Includes some individuals tested multiple times.`, client.user.avatarURL()) 
 
-                const $ = cheerio.load(body); 
+            const $ = cheerio.load(body); 
 
-                $('table[class="fixed_header"] > tbody > tr').each(function() {
-                    let stats = $(this).text(); 
-                    scuEmbed.addField(`\u200B`, stats, true);
-                });  
-                
-                message.channel.send(scuEmbed);
-            }
-        }); 
+            $('table[class="fixed_header"] > tbody > tr').each(function() {
+                let stats = $(this).text(); 
+                scuEmbed.addField(`\u200B`, stats, true);
+            });  
+
+            message.channel.send(scuEmbed);
+        } else {
+            sendMessage(client, client.config.channels.auditlogs, { embed: { description: `Couldn't retrieve data from the website. Check your code!`, color: client.config.school_color}}); 
+        }
     }
 }
