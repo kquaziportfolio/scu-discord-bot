b3942b1133bef908254b7e4d4ef2c40bb80cd4c8
Jason Vu <jvu@scu.edu>
2020-11-17 14:26:59 -0800

    Delete gitreload.js

--- commands/admin/gitreload.js
+++
@@ -1,8 +0,0 @@
-let isAdmin = require(`../../modules/isAdmin.js`);
-module.exports={
-name:"gitreload",
-description:"Git pull and restart",
-cooldown=20,
-catagory: "Admin",
-async execute(client,message,args){}
-}
