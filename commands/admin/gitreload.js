82b13d4aaae3674ea05af7280a66a520d6e209fd
kairanaquazi <48654706+kairanaquazi@users.noreply.github.com>
2020-11-17 13:36:34 -0800

    Create gitreload.js

---
+++ commands/admin/gitreload.js
@@ -0,0 +1,8 @@
+let isAdmin = require(`../../modules/isAdmin.js`);
+module.exports={
+name:"gitreload",
+description:"Git pull and restart",
+cooldown=20,
+catagory: "Admin",
+async execute(client,message,args){}
+}
