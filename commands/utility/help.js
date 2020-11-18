61f32211645cdd1da3286868406bd167ce822665
Jason Vu <jvu@scu.edu>
2020-11-18 11:43:33 -0800

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -13,11 +13,7 @@ module.exports = {
 		if (args[0]) {
 			const name = args[0];
 			const command = commands.get(name);
-			
-			if (!command) {
-				return message.reply({ embed: { description: `That\'s not a valid command!`, color: client.config.school_color}});
-			}
-			
+			 
 			const secHelpEmbed = new MessageEmbed()
 			.setTitle(`${command.name.toUpperCase()} Command`)
 			.addFields(
@@ -31,19 +27,21 @@ module.exports = {
 			.setFooter(`Use ${client.config.prefix}help [command name] to get specific commmand info!`) 
 			
 			return message.channel.send(secHelpEmbed);
-		}
+		} else {
+			return message.reply({ embed: { description: `That\'s not a valid command!`, color: client.config.school_color}});
+		}	
 
 		const mainHelpEmbed = new MessageEmbed()
-			.setTitle(`**SCU BOT COMMANDS**`)
-			.setDescription(`<@${client.user.id}> is the **SCU Discord Network**'s very own bot! :robot:\n\n` +
-			 `**Commands**\nA full list of commands is available [here](${client.config.verification.githubLink}tree/master/commands) or use \`${client.config.prefix}help [command name]\` to get specific command info!\n\n` +
-			 `**Support**\n[Click here](${client.config.verification.githubLink}pulls) to talk to our support team and/or suggest new ideas!`)
-			.setFooter(`Brought to you by the server lords!`)
-			.setColor(client.config.school_color)
-			.attachFiles([`./assets/logo-pic.png`])
-			.setThumbnail(`attachment://logo-pic.png`)
-		        .setURL(`${client.config.verification.githubLink}tree/master/commands`)
-			.setTimestamp()
+		.setTitle(`**SCU BOT COMMANDS**`)
+		.setDescription(`<@${client.user.id}> is the **SCU Discord Network**'s very own bot! :robot:\n\n` +
+		 `**Commands**\nA full list of commands is available [here](${client.config.verification.githubLink}tree/master/commands) or use \`${client.config.prefix}help [command name]\` to get specific command info!\n\n` +
+		 `**Support**\n[Click here](${client.config.verification.githubLink}pulls) to talk to our support team and/or suggest new ideas!`)
+		.setFooter(`Brought to you by the server lords!`)
+		.setColor(client.config.school_color)
+		.attachFiles([`./assets/logo-pic.png`])
+		.setThumbnail(`attachment://logo-pic.png`)
+		.setURL(`${client.config.verification.githubLink}tree/master/commands`)
+		.setTimestamp()
 
 		message.channel.send(mainHelpEmbed);
 	}
