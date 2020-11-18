ae8bbb87b28315863774814d6af0f9423a565e99
Jason Vu <jvu@scu.edu>
2020-11-18 11:50:10 -0800

    Update git-pull.js

--- commands/admin/git-pull.js
+++ commands/admin/git-pull.js
@@ -1,4 +1,5 @@
 let isAdmin = require(`../../modules/isAdmin.js`); 
+let sendMessage = require(`../../modules/sendMessage.js`);
 const child_proc = require('child_process');
  
 module.exports = {
@@ -11,14 +12,18 @@ module.exports = {
             try {
                 const frames = ['□', '□□□□ 25%', '□□□□□□□□ 50%', '□□□□□□□□□□□□ 75%', '□□□□□□□□□□□□□□□□ 100%'];
 	        const msg= await message.channel.send("Pulling...");
+		    
 		child_proc.exec("git pull origin master");
+		    
                 for (const frame of frames) {
-                    setTimeout(() => {}, 1000);
+                    setTimeout(() => {}, 4000);
                     await msg.edit({ embed: { description: frame, color: client.config.school_color}});
 		}
             } catch (err) {
                 console.log(err.message);
-            }
+            } finally {
+		sendMessage(client, client.config.channels.auditlogs, { embed: { description: `Finished pulling from [scu-discord-bot](client.config.verification.githubLink)!, color: client.config.school_color}}); 
+	    }
         }
     }
 } 
