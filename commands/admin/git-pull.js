55fbc3057322d55b4fe9cee16b4ea025bba2a6fa
kairanaquazi <48654706+kairanaquazi@users.noreply.github.com>
2020-11-18 09:41:48 -0800

    Update and rename git-reload.js to git-pull.js

--- commands/admin/git-pull.js
+++ commands/admin/git-pull.js
@@ -4,18 +4,16 @@ const child_proc = require('child_process');
 
 
 module.exports = {
-    name:"git-reload",
-    description: "Git pulls from my repo and restarts the bot from Discord!",
+    name:"git-pull",
+    description: "Git pulls from my repo!",
     cooldown: 20,
     category: "Admin",
     async execute(client,message,args) {
         if (isAdmin(client, message)) {
             try {
                 await message.channel.send("Pulling from the repo...");
-                child_proc.exec("git pull");
+                const msg=child_proc.exec("git pull");
                 const frames = ['□', '□□□□ 25%', '□□□□□□□□ 50', '□□□□□□□□□□□□ 75%', '□□□□□□□□□□□□□□□□ 100%'];
-
-                const msg = await message.channel.send(`Restarting the bot...`);
                 
                 for (const frame of frames) {
                     setTimeout(() => {}, 4000);
@@ -23,8 +21,6 @@ module.exports = {
 		}
             } catch (err) {
                 console.log(err.message);
-            } finally {
-                process.exit();
             }
         }
     }
