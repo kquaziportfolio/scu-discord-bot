c07f2d85002d9fd2d8c5ad3d86795b9be57cc5a0
Jason Vu <jvu@scu.edu>
2020-11-18 09:57:21 -0800

    Update git-pull.js

--- commands/admin/git-pull.js
+++ commands/admin/git-pull.js
@@ -1,8 +1,6 @@
-let isAdmin = require(`../../modules/isAdmin.js`);
-let sendMessage = require(`../../modules/sendMessage`);
+let isAdmin = require(`../../modules/isAdmin.js`); 
 const child_proc = require('child_process');
-
-
+ 
 module.exports = {
     name:"git-pull",
     description: "Git pulls from my repo!",
@@ -11,8 +9,8 @@ module.exports = {
     async execute(client,message,args) {
         if (isAdmin(client, message)) {
             try {
-                await message.channel.send("Pulling from the repo...");
-                const msg=child_proc.exec("git pull");
+                await message.channel.send({ embed: { description: "Pulling from the repo...", color: client.config.school_color}});
+                const msg= child_proc.exec("git pull");
                 const frames = ['□', '□□□□ 25%', '□□□□□□□□ 50', '□□□□□□□□□□□□ 75%', '□□□□□□□□□□□□□□□□ 100%'];
                 
                 for (const frame of frames) {
@@ -24,4 +22,4 @@ module.exports = {
             }
         }
     }
-}
+} 
