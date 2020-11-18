b8e1606dbb68814b095a3ff5b11bc3343ae69417
kairanaquazi <48654706+kairanaquazi@users.noreply.github.com>
2020-11-18 09:27:48 -0800

    Update git-reload.js

--- commands/admin/git-reload.js
+++ commands/admin/git-reload.js
@@ -1,6 +1,15 @@
 let isAdmin = require(`../../modules/isAdmin.js`);
 let sendMessage = require(`../../modules/sendMessage`);
+var exec = require('child_process').exec, child;
 
+child = exec('git pull',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
 module.exports = {
     name:"git-reload",
     description: "Git pulls from my repo and restarts the bot from Discord!",
@@ -8,12 +17,22 @@ module.exports = {
     category: "Admin",
     async execute(client,message,args) {
         if (isAdmin(client, message)) {
-            //execute code here
-            
-            //AUDIT LOGGER!!
-            /*
-            sendMessage(client, client.config.channels.auditlog, /*insert message embed or regular text here for content*/);
-            */
+            try {
+                await message.channel.send("Pulling from the repo...");
+                child();
+                const frames = ['□', '□□□□ 25%', '□□□□□□□□ 50', '□□□□□□□□□□□□ 75%', '□□□□□□□□□□□□□□□□ 100%'];
+
+                const msg = await message.channel.send(`Restarting the bot...`);
+                
+                for (const frame of frames) {
+                    setTimeout(() => {}, 4000);
+                    await msg.edit({ embed: { description: frame, color: client.config.school_color}});
+		        }
+            } catch (err) {
+                console.log(err.message);
+            } finally {
+                process.exit();
+            }
         }
     }
 }
