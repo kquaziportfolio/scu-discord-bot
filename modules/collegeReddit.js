af3ab5119200a05e63585aecc15ecb641cbed408
Jason Vu <jvu@scu.edu>
2020-11-18 08:56:47 -0800

    Update collegeReddit.js

--- modules/collegeReddit.js
+++ modules/collegeReddit.js
@@ -1,40 +1,38 @@
 
 module.exports.run = async (client) => {
+  const { MessageEmbed } = require(`discord.js`);
+  const request = require('request');
+  const entities = require('entities');
+  const validUrl = require('valid-url');
+  const sendMessage = require(`../modules/sendMessage.js`);
+  const botReady = true; 
 
-const { MessageEmbed } = require(`discord.js`);
-const request = require('request');
-const entities = require('entities');
-const validUrl = require('valid-url');
-const sendMessage = require(`../modules/sendMessage.js`);
-const botReady = true;
-const subredditUrl = `https://www.reddit.com/r/college/new.json?limit=5`;
+  setInterval(() => {
+    if (botReady) {
+      request({
+        url: client.config.api.subreddit,
+        json: true,
+      }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          for (const post of body.data.children.reverse()) {
+            const lastTimestamp = post.data.created_utc;
+            if (lastTimestamp <= post.data.created_utc) {
+              const redditPost = new MessageEmbed()
+              .setColor(client.config.school_color)
+              .setTitle(`${post.data.link_flair_text ? `[${post.data.link_flair_text}] ` : ''}${entities.decodeHTML(post.data.title)}`)
+              .setURL(`https://redd.it/${post.data.id}`)
+              .setDescription(post.data.selftext)
+              .setThumbnail(validUrl.isUri(post.data.thumbnail) ? entities.decodeHTML(post.data.thumbnail) : null)
+              .setFooter(`${post.data.is_self ? 'self post' : 'link post'} by ${post.data.author}`)
+              .setTimestamp(new Date(post.data.created_utc * 1000))
 
-setInterval(() => {
-  if (botReady) {
-    request({
-      url: subredditUrl,
-      json: true,
-    }, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        for (const post of body.data.children.reverse()) {
-          const lastTimestamp = post.data.created_utc;
-          if (lastTimestamp <= post.data.created_utc) {
-            const redditPost = new MessageEmbed()
-            .setColor(client.config.school_color)
-            .setTitle(`${post.data.link_flair_text ? `[${post.data.link_flair_text}] ` : ''}${entities.decodeHTML(post.data.title)}`)
-            .setURL(`https://redd.it/${post.data.id}`)
-            .setDescription(`${post.data.is_self ? entities.decodeHTML(post.data.selftext.length > 253 ? post.data.selftext.slice(0, 253).concat('...') : post.data.selftext) : ''}`)
-            .setThumbnail(validUrl.isUri(post.data.thumbnail) ? entities.decodeHTML(post.data.thumbnail) : null)
-            .setFooter(`${post.data.is_self ? 'self post' : 'link post'} by ${post.data.author}`)
-            .setTimestamp(new Date(post.data.created_utc * 1000))
-
-            sendMessage(client, "778323568580755456", redditPost);
+              sendMessage(client, client.config.channels.reddit, redditPost);
+            }
           }
+        } else {
+          sendMessage(client, client.config.channels.auditlogs, 'Request failed - reddit could be down or subreddit doesn\'t exist. Will continue.');
         }
-      } else {
-        sendMessage(client, client.config.channels.auditlogs, 'Request failed - reddit could be down or subreddit doesn\'t exist. Will continue.');
-      }
-    });
-  }
-}, 1800 * 1000); // get 5 new posts every 30 minutes!
+      });
+    }
+  }, 1800 * 1000); // get 5 new posts every 30 minutes!
 }
