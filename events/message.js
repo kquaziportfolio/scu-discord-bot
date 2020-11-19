b128ba630340531aafe4c0dfa63c1d0f3c27bdaf
Jason Vu <jvu@scu.edu>
2020-11-19 09:38:00 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -7,7 +7,7 @@ const { prefix } = require(`../config.json`);
 
 module.exports = async (client, message) => { 
   // Checks if the Author is a Bot, or the message isn't from the guild, ignore it.
-  if (!message.content.startsWith(client.config.prefix) && message.channel.type != "dm" || message.author.bot) return;
+  if (message.author.bot) return;
 
 /*
 ===============================================   
