dba8dd537e0adff7ac1a8b424c5d585373170615
Jason Vu <jvu@scu.edu>
2020-11-19 14:12:15 -0800

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -10,10 +10,6 @@ module.exports = {
     async execute(client, message, args) {
         if(isAdmin(client, message)) {
           let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8")); //Read File
-          
-          prefixes[message.guild.id] = { //Let The config be
-              prefix: args[0] //Let prefix = argument 1
-          }
 
           fs.writeFile("./prefix.json", JSON.stringify(prefixes), (err) => { //Write File
             if(err) console.log(err); //If error log error to the console
@@ -26,6 +22,10 @@ module.exports = {
           if (args[1] || args[0].length > 1) {
             return message.channel.send({ embed: { description: `You can't set a double-argument prefix or one that's over 1 character!`, color: client.config.school_color}});
           } 
+            
+          prefixes[message.guild.id] = { //Let The config be
+              prefix: args[0] //Let prefix = argument 1
+          }
 
           message.channel.send({ embed: { description: `Set the bot prefix to ${args[0]}!`, color: client.config.school_color}});
         }
