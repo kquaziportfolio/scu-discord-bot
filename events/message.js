18a102db5975b6735d5572b9c2cae10dd1e3e199
Jason Vu <jvu@scu.edu>
2020-11-19 10:51:58 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,14 +1,86 @@
 const { MessageEmbed, Collection } = require(`discord.js`);  
 let isAdmin = require(`../modules/isAdmin.js`);
-const db = require(`quick.db`);
+const fs = require(`fs`);
 let sendMessage = require(`../modules/sendMessage.js`);
 const cooldowns = new Collection()  
 
 module.exports = async (client, message) => {    
   // Checks if the Author is a Bot, or the message isn't from the guild, ignore it.
   if (message.content.indexOf(client.config.prefix) !== 0 || message.author.bot) return;
-	  
+	
+  let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8")); //Read File
+  if(!prefixes[msg.guild.id]) {  //If there is no string that is startwith prefixes[msg.guild.id]
+    prefixes[msg.guild.id] = { //Let prefixes[msg.guild.id] be
+      prefix: client.config.prefix //Prefix = Default Prefix Which is on config.json
+    }
+  }
+
+  let otherPrefix = prefixes[msg.guild.id].prefix; //Let prefix be prefixes[msg.guild.id].prefix
+       
 /*
+==================================================================================
+  __  __                                  _    _                 _ _           
+ |  \/  |                                | |  | |               | | |          
+ | \  / | ___  ___ ___  __ _  __ _  ___  | |__| | __ _ _ __   __| | | ___ _ __ 
+ | |\/| |/ _ \/ __/ __|/ _` |/ _` |/ _ \ |  __  |/ _` | '_ \ / _` | |/ _ \ '__|
+ | |  | |  __/\__ \__ \ (_| | (_| |  __/ | |  | | (_| | | | | (_| | |  __/ |   
+ |_|  |_|\___||___/___/\__,_|\__, |\___| |_|  |_|\__,_|_| |_|\__,_|_|\___|_|   
+                              __/ |                                            
+                             |___/                                             
+==================================================================================
+*/
+
+  // Our standard argument/command name definition. 
+  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g) || message.content.slice(otherPrefix.length).trim().split(/ +/g);
+  const commandName = args.shift().toLowerCase();
+
+  // Grab the command data from the client.commands Enmap
+  const command = client.commands.get(commandName);
+
+  // If that command doesn't exist, return nothing
+  if (!command) return;
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, <@${message.author.id}>!`;
+    
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${client.config.prefix}${command.name} ${command.usage}\``;
+    }
+    
+    return message.channel.send({embed: { title: "Uh-oh :x:", description: reply, color: client.config.school_color}});
+  }
+	
+/*
+=======================================================
+  / ____|          | |   | |                        
+ | |     ___   ___ | | __| | _____      ___ __  ___ 
+ | |    / _ \ / _ \| |/ _` |/ _ \ \ /\ / / '_ \/ __|
+ | |___| (_) | (_) | | (_| | (_) \ V  V /| | | \__ \
+  \_____\___/ \___/|_|\__,_|\___/ \_/\_/ |_| |_|___/
+=======================================================
+*/                                                    
+                                                     
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Collection());
+  }
+  
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000; //make cooldown default to 3 if there are no presets for cooldown in the command
+  
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply({ embed: { description: `Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`, color: client.config.school_color}});
+    }
+  }
+  
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+  
+  /*
 ===============================================   
  |  \/  |         | |               (_) | 
  | \  / | ___   __| |_ __ ___   __ _ _| | 
@@ -114,70 +186,6 @@ module.exports = async (client, message) => {
       } 
     }
   }  
-       
-/*
-==================================================================================
-  __  __                                  _    _                 _ _           
- |  \/  |                                | |  | |               | | |          
- | \  / | ___  ___ ___  __ _  __ _  ___  | |__| | __ _ _ __   __| | | ___ _ __ 
- | |\/| |/ _ \/ __/ __|/ _` |/ _` |/ _ \ |  __  |/ _` | '_ \ / _` | |/ _ \ '__|
- | |  | |  __/\__ \__ \ (_| | (_| |  __/ | |  | | (_| | | | | (_| | |  __/ |   
- |_|  |_|\___||___/___/\__,_|\__, |\___| |_|  |_|\__,_|_| |_|\__,_|_|\___|_|   
-                              __/ |                                            
-                             |___/                                             
-==================================================================================
-*/
-
-  // Our standard argument/command name definition.
-  const otherPrefix = db.get(`guild_${message.guild.id}_prefix`);	
-  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g) || message.content.slice(otherPrefix.length).trim().split(/ +/g);
-  const commandName = args.shift().toLowerCase();
-
-  // Grab the command data from the client.commands Enmap
-  const command = client.commands.get(commandName);
-
-  // If that command doesn't exist, return nothing
-  if (!command) return;
-
-  if (command.args && !args.length) {
-    let reply = `You didn't provide any arguments, <@${message.author.id}>!`;
-    
-    if (command.usage) {
-      reply += `\nThe proper usage would be: \`${client.config.prefix}${command.name} ${command.usage}\``;
-    }
-    
-    return message.channel.send({embed: { title: "Uh-oh :x:", description: reply, color: client.config.school_color}});
-  }
-	
-/*
-=======================================================
-  / ____|          | |   | |                        
- | |     ___   ___ | | __| | _____      ___ __  ___ 
- | |    / _ \ / _ \| |/ _` |/ _ \ \ /\ / / '_ \/ __|
- | |___| (_) | (_) | | (_| | (_) \ V  V /| | | \__ \
-  \_____\___/ \___/|_|\__,_|\___/ \_/\_/ |_| |_|___/
-=======================================================
-*/                                                    
-                                                     
-  if (!cooldowns.has(command.name)) {
-    cooldowns.set(command.name, new Collection());
-  }
-  
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.name);
-  const cooldownAmount = (command.cooldown || 3) * 1000; //make cooldown default to 3 if there are no presets for cooldown in the command
-  
-  if (timestamps.has(message.author.id)) {
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-    if (now < expirationTime) {
-      const timeLeft = (expirationTime - now) / 1000;
-      return message.reply({ embed: { description: `Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`, color: client.config.school_color}});
-    }
-  }
-  
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
   
   try {
   // Run the command as long as it has these three parameters
