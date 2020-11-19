6c60648d451cd71a447ba1eb12c4fb17ff295d8c
Jason Vu <jvu@scu.edu>
2020-11-19 14:13:55 -0800

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -10,24 +10,20 @@ module.exports = {
     async execute(client, message, args) {
         if(isAdmin(client, message)) {
           let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8")); //Read File
-
-          fs.writeFile("./prefix.json", JSON.stringify(prefixes), (err) => { //Write File
-            if(err) console.log(err); //If error log error to the console
-          })
-            
-          if (args[0] == prefixes[message.guild.id].prefix) {
-            return message.channel.send({ embed: { description: `This is the current prefix you already have!`, color: client.config.school_color}});
-          }
-            
-          if (args[1] || args[0].length > 1) {
-            return message.channel.send({ embed: { description: `You can't set a double-argument prefix or one that's over 1 character!`, color: client.config.school_color}});
-          } 
             
           prefixes[message.guild.id] = { //Let The config be
               prefix: args[0] //Let prefix = argument 1
           }
 
-          message.channel.send({ embed: { description: `Set the bot prefix to ${args[0]}!`, color: client.config.school_color}});
+          fs.writeFile("./prefix.json", JSON.stringify(prefixes), (err) => { //Write File
+            if(err) console.log(err); //If error log error to the console
+          })
+             
+          if (args[1] || args[0].length > 1) {
+            return message.channel.send({ embed: { description: `You can't set a double-argument prefix or one that's over 1 character!`, color: client.config.school_color}});
+          } 
+             
+          message.channel.send({ embed: { description: `Set the bot prefix to \`${args[0]}!\``, color: client.config.school_color}});
         }
     }
 }
