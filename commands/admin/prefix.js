2f4b3f9ed7cd4968b77c60009662ff8b1b035b2f
Jason Vu <jvu@scu.edu>
2020-11-19 10:52:00 -0800

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -1,5 +1,5 @@
 let isAdmin = require(`../../modules/isAdmin.js`);
-let db = require(`quick.db`); 
+let fs = require(`fs`); 
 
 module.exports = { 
     name: 'prefix',
@@ -9,16 +9,19 @@ module.exports = {
     category: 'Admin',  
     async execute(client, message, args) {
         if(isAdmin(client, message)) {
-          if (args[0] === db.get(`guild_${message.guild.id}_prefix`)) {
-            return message.channel.send({ embed: { description: `\`${args[0]}\` is already your current prefix!`, color: client.config.school_color}});
+          let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8")); //Read File
+            
+          prefixes[msg.guild.id] = { //Let The config be
+            prefix: args[0] //Let prefix = arguement 1
           }
+
+          fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => { //Write File
+            if(err) console.log(err); //If error log error to the console
+          })
             
           if (args[1] || args[0].length > 1) {
             return await message.channel.send({ embed: { description: `You can't set a double-argument prefix or one that's over 1 character!`, color: client.config.school_color}});
-          }
-            
-          db.delete(`guild_${message.guild.id}_prefix`);
-          db.set(`guild_${message.guild.id}_prefix`, args[0]);
+          } 
 
           await message.channel.send({ embed: { description: `Set the bot prefix to ${args[0]}!`, color: client.config.school_color}});
         }
