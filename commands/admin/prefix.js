be50df74d0a64657bedd56ebec561f0ca46326f8
Jason Vu <jvu@scu.edu>
2020-11-19 15:10:31 -0800

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -10,20 +10,25 @@ module.exports = {
     async execute(client, message, args) {
         if(isAdmin(client, message)) {
           let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8")); //Read File
-            
-          prefixes[message.guild.id] = { //Let The config be
-              prefix: args[0] //Let prefix = argument 1
+             
+          if (args[1] || args[0].length > 1) {
+            return message.channel.send({ embed: { description: `:x: You can't set a double-argument prefix or one that's greater than one character!`, color: `RED`}});
+          } else if (args[0] == prefixes[message.guild.id].prefix) { //detects if input resembles the default value in the config.json
+            return message.channel.send({ embed: { description: `:x: You can't set the prefix equal to its default value!`, color: `RED`}});
+          } else if (args[0].match(/^[a-zA-Z]+$/)) { //detects if character is from alphabet, in either lowercase/uppercase form, will return nothing
+            return message.channel.send({ embed: { description: `:x: You can't use any letters in the alphabet!`, color: `RED`}});
           }
 
+          prefixes[message.guild.id] = { //Let The config be
+            prefix: args[0] //Let prefix = argument 1
+          }
+            
           fs.writeFile("./prefix.json", JSON.stringify(prefixes), (err) => { //Write File
             if(err) console.log(err); //If error log error to the console
           })
-             
-          if (args[1] || args[0].length > 1 || args[0 == prefixes[message.guild.id].prefix]) {
-            return message.channel.send({ embed: { description: `You can't set a double-argument prefix or one that's greater than one character!`, color: client.config.school_color}});
-          } 
-             
+
           message.channel.send({ embed: { description: `Set the bot prefix to \`${args[0]}\`!`, color: client.config.school_color}});
+        
         }
     }
 }
