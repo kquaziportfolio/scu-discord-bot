92244aeaceada63030c8ef96cc5f371d960d2ae6
Jason Vu <jvu@scu.edu>
2020-11-18 22:04:17 -0800

    Update collegeReddit.js

--- modules/collegeReddit.js
+++ modules/collegeReddit.js
@@ -1,3 +1,15 @@
+/*
+=====================================================
+     __     __  _____       _ _                  
+    / /    / / /  __ \     | | |                 
+   / / __ / /  | /  \/ ___ | | | ___  __ _  ___  
+  / / '__/ /   | |    / _ \| | |/ _ \/ _` |/ _ \ 
+ / /| | / /    | \__/\ (_) | | |  __/ (_| |  __/ 
+/_/ |_|/_/      \____/\___/|_|_|\___|\__, |\___| 
+                                      __/ |      
+                                     |___/       
+=====================================================
+*/
 
 module.exports.run = async (client) => {
   const { MessageEmbed } = require(`discord.js`);
@@ -29,24 +41,23 @@ module.exports.run = async (client) => {
             if (lastTimestamp <= post.data.created_utc) {
               const redditPost = new MessageEmbed()
               .setColor(client.config.school_color)
-              .setAuthor(`${post.data.subreddit_name_prefixed}`, client.user.displayAvatarURL())
+              .setAuthor(`/${post.data.subreddit_name_prefixed}`, client.user.displayAvatarURL())
               .setTitle(`${post.data.link_flair_text ? `[${post.data.link_flair_text}] ` : ''}${entities.decodeHTML(post.data.title)}`)
               .setURL(`https://redd.it/${post.data.id}`)
-              .setDescription(post.data.selftext)
               .setThumbnail(validUrl.isUri(post.data.thumbnail) ? entities.decodeHTML(post.data.thumbnail) : null)
-              .setFooter(`${post.data.is_self ? 'self post' : 'link post'} by ${post.data.author}`)
+              .setFooter(`${post.data.is_self ? 'self post' : 'link post'} by /u/${post.data.author}`)
               .setTimestamp(new Date(post.data.created_utc * 1000))
               
               if (post.data.selftext.length > 2048) { //if reddit post exceeds 2048 characters, split the message into two embeds to render the content :)
-                sendMessage(client, client.config.channels.reddit, { embed: { description: post.data.selftext.substring(0, 2047), color: client.config.school_color}}); 
-                redditPost.setDescription(post.data.selftext.substring(2048, post.data.selftext.length));
+                redditPost.setDescription(post.data.selftext.substring(0, 2047));
+                sendMessage(client, client.channels.auditlogs, { embed: { description: post.data.selftext.substring(2048, post.data.selftext.length)}});
               }
 
               sendMessage(client, client.config.channels.reddit, redditPost);
             }
           }
         } else {
-          sendMessage(client, client.config.channels.auditlogs, { embed: { description: `Request failed - reddit could be down or subreddit doesn't exist. Will continue.`, color: client.config.school_color}});
+          sendMessage(client, client.config.channels.auditlogs, { embed: { description: `Request failed - Reddit could be down or the subreddit doesn't exist. Will continue.`, color: client.config.school_color}});
         }
       });
     }
