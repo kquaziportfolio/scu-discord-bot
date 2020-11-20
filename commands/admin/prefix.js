25efb5157f7570e50598a9e28429715fc0c299e9
Jason Vu <jvu@scu.edu>
2020-11-20 09:20:22 -0800

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -5,27 +5,32 @@ module.exports = {
     name: 'prefix',
     description: 'Change the bot\'s prefix!',
     args: true,
+    cooldown: 10,
     usage: `[enter prefix]`,
     category: 'Admin',  
-    cooldown: 10,
-    async execute(client, message, args) { 
-        if(isAdmin(client, message)) {  
-          const defaultPrefix = JSON.parse(fs.readFileSync('./config.json')); 
-          
-          defaultPrefix.prefix = args[0];
-
+    async execute(client, message, args) {
+        if(isAdmin(client, message)) { 
+          let defaultPrefix = JSON.parse(fs.readFileSync("./config.json", "utf8")); //Read File
+          if(defaultPrefix) {  
+            defaultPrefix.prefix //Prefix = Default Prefix Which is on config.json
+          } 
+             
           if (args[1] || args[0].length > 1) {
             return message.channel.send({ embed: { description: `:x: You can't set a double-argument prefix or one that's greater than one character!`, color: `RED`}});
+          } else if (args[0] == defaultPrefix.prefix) { //detects if input resembles the default value in the config.json
+            return message.channel.send({ embed: { description: `:x: You can't set the prefix equal to its default value!`, color: `RED`}});
           } else if (args[0].match(/^[a-zA-Z]+$/)) { //detects if character is from alphabet, in either lowercase/uppercase form, will return nothing
             return message.channel.send({ embed: { description: `:x: You can't use any letters in the alphabet!`, color: `RED`}});
-          } 
-            
-          fs.writeFileSync('./config.json', JSON.stringify(defaultPrefix), 'utf-8');
-            
-          let msg = await message.channel.send({ embed: { description: `Set the bot prefix to \`${args[0]}\`!`, color: client.config.school_color}});
-          await msg.edit({embed: { description: `Wait a bit while I readjust!`, color: client.config.school_color}})
+          }
 
-          //process.exit(); 
+          defaultPrefix.prefix = args[0]; //Let the file be read and let prefix = argument 1
+            
+          fs.writeFile("./config.json", JSON.stringify(defaultPrefix), (err) => { //Write File
+            if(err) console.log(err); //If error log error to the console
+          })
+
+          message.channel.send({ embed: { description: `Set the bot prefix to \`${args[0]}\`!`, color: client.config.school_color}});
+        
         }
     }
 }
