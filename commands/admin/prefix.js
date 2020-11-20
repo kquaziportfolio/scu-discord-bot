e87da69b4fc1c693c7ccfaa90a9f9a79a3e18ec5
Jason Vu <jvu@scu.edu>
2020-11-19 21:34:40 -0800

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -8,13 +8,9 @@ module.exports = {
     usage: `[enter prefix]`,
     category: 'Admin',  
     async execute(client, message, args) {
-        if(isAdmin(client, message)) { 
-          let defaultPrefix = JSON.parse(fs.readFileSync("./config.json", "utf8")); //Read File
-          if(defaultPrefix) {  //If there is no string that is startwith prefixes[message.guild.id]
-            defaultPrefix = { //Let prefixes[message.guild.id] be
-              prefix: client.config.prefix //Prefix = Default Prefix Which is on config.json
-            }
-          } 
+        if(isAdmin(client, message)) {  
+          const defaultPrefix = JSON.parse(fs.readFileSync('./config.json'))
+          defaultPrefix.prefix = args[0]
              
           if (args[1] || args[0].length > 1) {
             return message.channel.send({ embed: { description: `:x: You can't set a double-argument prefix or one that's greater than one character!`, color: `RED`}});
@@ -23,15 +19,8 @@ module.exports = {
           } else if (args[0].match(/^[a-zA-Z]+$/)) { //detects if character is from alphabet, in either lowercase/uppercase form, will return nothing
             return message.channel.send({ embed: { description: `:x: You can't use any letters in the alphabet!`, color: `RED`}});
           }
-
-          defaultPrefix = { //Let the file be read
-            prefix: args[0] //Let prefix = argument 1
-          }
-            
-          fs.writeFile("./config.json", JSON.stringify(defaultPrefix), (err) => { //Write File
-            if(err) console.log(err); //If error log error to the console
-          })
-
+ 
+          fs.writeFileSync('./config.json', JSON.stringify(defaultPrefix), 'utf-8')
           message.channel.send({ embed: { description: `Set the bot prefix to \`${args[0]}\`!`, color: client.config.school_color}});
         
         }
