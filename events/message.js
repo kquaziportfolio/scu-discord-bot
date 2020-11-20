95d70f08e3e36b0b8ee5058d47f06a6b07eb3984
Jason Vu <jvu@scu.edu>
2020-11-19 17:06:22 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -12,7 +12,7 @@ module.exports = async (client, message) => {
         prefix: client.config.prefix //Prefix = Default Prefix Which is on config.json
       }
 
-      let otherPrefix = prefixes[message.guild.id].prefix; //Let prefix be prefixes[message.guild.id].prefix
+      const otherPrefix = prefixes[message.guild.id].prefix; //Let prefix be prefixes[message.guild.id].prefix
 
       // Checks if the Author is a Bot, the prefix isn't right, or the message isn't from the guild, ignore it.
       if (!message.content.startsWith(otherPrefix) ||  message.author.bot) return;  
