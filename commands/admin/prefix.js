4fb83d5865ef9a56ca3c65e99365e6f6687b75b8
Jason Vu <jvu@scu.edu>
2020-11-20 09:04:41 -0800

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -14,6 +14,7 @@ module.exports = {
           const defaultPrefix = JSON.parse(fs.readFileSync('./config.json')); 
           
           defaultPrefix.prefix = args[0];
+          fs.writeFileSync('./config.json', JSON.stringify(defaultPrefix), 'utf-8');
 
           if (args[1] || args[0].length > 1) {
             return message.channel.send({ embed: { description: `:x: You can't set a double-argument prefix or one that's greater than one character!`, color: `RED`}});
@@ -21,10 +22,7 @@ module.exports = {
             return message.channel.send({ embed: { description: `:x: You can't set the prefix equal to its default value!`, color: `RED`}});
           } else if (args[0].match(/^[a-zA-Z]+$/)) { //detects if character is from alphabet, in either lowercase/uppercase form, will return nothing
             return message.channel.send({ embed: { description: `:x: You can't use any letters in the alphabet!`, color: `RED`}});
-          } else {
-
-              fs.writeFileSync('./config.json', JSON.stringify(defaultPrefix), 'utf-8');
-
+          } else { 
               let msg = await message.channel.send({ embed: { description: `Set the bot prefix to \`${args[0]}\`!`, color: client.config.school_color}});
               await msg.edit({embed: { description: `Wait a bit while I readjust!`, color: client.config.school_color}})
               
