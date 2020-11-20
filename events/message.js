8f5652e6060a23a0b93c361ad85fea142dd0ba4f
Jason Vu <jvu@scu.edu>
2020-11-19 16:41:10 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -6,7 +6,8 @@ let sendMessage = require(`../modules/sendMessage.js`);
 const cooldowns = new Collection()  
 
 module.exports = async (client, message) => {      
-     
+	
+      let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8")); //Read File
       if(message.guild != null && !prefixes[message.guild.id]) {  //If there is no string that includes prefixes[message.guild.id]
         prefixes[message.guild.id] = { //Let prefixes[message.guild.id] be
           prefix: client.config.prefix //Prefix = Default Prefix Which is on config.json
