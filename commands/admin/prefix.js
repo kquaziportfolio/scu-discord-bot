58caf31d8860840ff3a0fe7133cadef41f09b7ff
Jason Vu <jvu@scu.edu>
2020-11-19 22:59:14 -0800

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -7,22 +7,32 @@ module.exports = {
     args: true,
     usage: `[enter prefix]`,
     category: 'Admin',  
+    cooldown: 5,
     async execute(client, message, args) {
+      try {
         if(isAdmin(client, message)) {  
-          const defaultPrefix = JSON.parse(fs.readFileSync('./config.json'))
-          defaultPrefix.prefix = args[0]
-             
+          const defaultPrefix = JSON.parse(fs.readFileSync('./config.json')); 
+          
+          defaultPrefix.prefix = args[0];
+
           if (args[1] || args[0].length > 1) {
             return message.channel.send({ embed: { description: `:x: You can't set a double-argument prefix or one that's greater than one character!`, color: `RED`}});
-          } else if (args[0] == defaultPrefix.prefix) { //detects if input resembles the default value in the config.json
+          /*} else if (args[0] == defaultPrefix.prefix) { //detects if input resembles the default value in the config.json
             return message.channel.send({ embed: { description: `:x: You can't set the prefix equal to its default value!`, color: `RED`}});
-          } else if (args[0].match(/^[a-zA-Z]+$/)) { //detects if character is from alphabet, in either lowercase/uppercase form, will return nothing
+          */} else if (args[0].match(/^[a-zA-Z]+$/)) { //detects if character is from alphabet, in either lowercase/uppercase form, will return nothing
             return message.channel.send({ embed: { description: `:x: You can't use any letters in the alphabet!`, color: `RED`}});
-          }
- 
-          fs.writeFileSync('./config.json', JSON.stringify(defaultPrefix), 'utf-8')
-          message.channel.send({ embed: { description: `Set the bot prefix to \`${args[0]}\`!`, color: client.config.school_color}});
-        
+          } 
+
+          fs.writeFileSync('./config.json', JSON.stringify(defaultPrefix), 'utf-8');
+          
+          let msg = await message.channel.send({ embed: { description: `Set the bot prefix to \`${args[0]}\`!`, color: client.config.school_color}});
+          await msg.edit({embed: { description: `Wait a bit while I readjust!`, color: client.config.school_color}});
+
         }
+      } catch(err) {
+          console.log(err);
+      } finally {
+          process.exit();
+      }
     }
 }
