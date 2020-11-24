6de8623d0571191aeb44aff93f381185d3bd8504
Jason Vu <jvu@scu.edu>
2020-11-24 10:33:09 -0800

    Update questionofDay.js

--- modules/questionofDay.js
+++ modules/questionofDay.js
@@ -22,22 +22,26 @@ module.exports.run = async (client) => {
       }, (error, response, body) => {
         if (!error && response.statusCode === 200) { 
             const options = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
+            let questionString = "";
             body.results[0].incorrect_answers.forEach((choice, index) => {
-              questionString += `${options[index]}: ${choice || body.results[0].correct_answer}\n\n`;
-            });
+              questionString += `${options[index]}: ${choice}\n\n`;
+            });  
+            
+            questionString += `${options[3]}: ${body.results[0].correct_answer}`
+
             const questionPost = new MessageEmbed()
             .setColor(client.config.school_color)
             .setAuthor(`Question of the Day`, client.user.displayAvatarURL())
             .setTitle(body.results[0].question) 
             .setDescription(questionString)
             .setFooter(`From the Open Trivia Database!`)
-            .setTimestamp(new Date(post.data.created_utc * 1000))
+            .setTimestamp(new Date())
         
-            sendMessage(client, client.config.channels.questionofDay, questionPost); 
+            sendMessage(client, client.config.channels.questionofDay, questionPost);
         } else {
           sendMessage(client, client.config.channels.auditlogs, { embed: { description: `Request failed. Will continue.`, color: client.config.school_color}});
         }
       });
     }
-  }, 10000);  
+  }, 54000 * 1000);  
 }
