692f26d8a72d1f91fe0969bf76717f64d7157b4e
Jason Vu <jvu@scu.edu>
2020-11-26 13:00:55 -0800

    Update other-discords.js

--- commands/admin/other-discords.js
+++ commands/admin/other-discords.js
@@ -1,4 +1,3 @@
-let isAdmin = require("../../modules/isAdmin.js");
 let sendMessage = require("../../modules/sendMessage.js");
 
 module.exports = { 
@@ -7,13 +6,11 @@ module.exports = {
     args: true,
     usage: `[title] | [description] | [image url]`, 
     category: 'Admin',  
-    async execute(client, message, args) {
-        if(isAdmin(client, message)) {
+    async execute(client, message, args) { 
             const prompt = args.join(' ').split('|');
                     
             sendMessage(client, client.config.channels.promos, {embed : {color: client.config.school_color, title: `${prompt[0]}`, description: `${prompt[1]}`, thumbnail: {url: `${prompt[2]}`}}});
 
             sendMessage(client, client.config.channels.auditlogs, { embed: { title: `__**Discord Promo Created!**__`, description: `<@${message.author.id}> just created a Discord server promo!`}})
-        }
     }
 }
