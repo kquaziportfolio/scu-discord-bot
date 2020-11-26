1f2c4934c48f38712ee81c7630ee1ac5a9542ba2
Jason Vu <jvu@scu.edu>
2020-11-26 13:01:47 -0800

    Update purge.js

--- commands/admin/purge.js
+++ commands/admin/purge.js
@@ -1,20 +1,16 @@
-let isAdmin = require(`../../modules/isAdmin.js`);
-
-module.exports = {
+ module.exports = {
     name: 'purge',
     description: 'Delete recent messages using this command!',
     args: true,
     usage: `[# of messages]`, 
     category: 'Admin',  
-    async execute(client, message, args) {   
-        if(isAdmin(client, message)) {
+    async execute(client, message, args) {    
             try {
                 const deleteCount = parseInt(args[0], 10);
        
                 await message.channel.bulkDelete(deleteCount + 1)
             } catch(e) {
                 console.log(e);
-            }
-        } 
+            } 
     }
 }
