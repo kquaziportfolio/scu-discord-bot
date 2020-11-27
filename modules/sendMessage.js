08d0c932e6230608169156d8dfcef0367af196e4
Jason Vu <jvu@scu.edu>
2020-11-26 21:58:17 -0800

    Update sendMessage.js

--- modules/sendMessage.js
+++ modules/sendMessage.js
@@ -10,11 +10,11 @@
 */
 
 module.exports = async function sendMessage(client, channel, content) {
-    client.guilds.cache.map((g) => {
-      try {
-        g.channels.cache.find((ch) => ch.id === channel).send(content);
-      } catch (e) {
-          return;
-      }
-    });
+  client.guilds.cache.map((g) => {
+    try { //get channel either by ID or name and send the content! 
+      g.channels.cache.find((ch) => ch.id == channel || ch.name == channel).send(content);
+    } catch (e) {
+      return;
+    }
+  });
 }
