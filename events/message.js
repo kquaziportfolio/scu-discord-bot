069d5318ab934a176cee42aaf77a576502dfaa60
Jason Vu <jvu@scu.edu>
2020-11-29 09:03:05 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -119,7 +119,7 @@ module.exports = async (client, message) => {
     const isPause = await db.get(`suspended${support.targetID}`);
     const modmailArgs = message.content.split(" ").slice(1);  
 
-      if (guildRole.modRoles.forEach(modRole => !(message.member.roles.cache.has(modRole))) || message.author.id !== guildRole.botOwner)) {
+      if (guildRole.modRoles.forEach(modRole => !(message.member.roles.cache.has(modRole))) || message.author.id !== guildRole.botOwner) {
           message.delete(); 
           message.channel.send(`<@${message.author.id}>`, { embed: { description: `You don't have one of the following roles: \`OWNER\`, \`ADMIN\`, \`MOD\``, color: client.config.school_color}});
           return false;
